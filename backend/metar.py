3434131a78dee67eef78a2deed969e25eb4a6367
Vladimir Vrzić <vvrzic@gmail.com>
2020-08-29 17:35:28 +0200

    Filter METAR stations by IATA id

--- backend/metar.py
+++ backend/metar.py
@@ -2,7 +2,10 @@
 
 import json
 import requests
+import sys
 
+
+EXIT_SUCCESS = 0
 
 HEADER = 'METAR'
 
@@ -33,12 +36,12 @@ def parse_json(content):
     for feature in weather['features']:
         report = {'data': []}
         properties = feature['properties']
-        if 'cover' in properties:
-            report['condition'] = get_condition_code(properties['cover'])
         if 'site' in properties:
+            report['site'] = properties['site']
             site = ('Site', properties['site'], '')
             report['data'].append(site)
         if 'id' in properties:
+            report['id'] = properties['id']
             ident = ('IATA id', properties['id'], '')
             report['data'].append(ident)
         if 'obsTime' in properties:
@@ -56,11 +59,31 @@ def parse_json(content):
         if 'wdir' in properties:
             wdir = ('Wind direction', properties['wdir'], '°')
             report['data'].append(wdir)
+        if 'cover' in properties:
+            report['condition'] = get_condition_code(properties['cover'])
+            condition = ('Cloud cover', properties['cover'], '')
+            report['data'].append(condition)
         # sanity check
         if 'temp' in properties:
             reports.append(report)
     return reports
 
 
-def parse_args(_args):
-    return 'metar', []
+def filter_reports(all_reports, stations):
+    station_set = set(map(lambda x: x.upper(), stations))
+    return list(filter(lambda x: x['id'] in station_set, all_reports)) if stations \
+        else all_reports
+
+
+def print_stations_list():
+    reports = parse_json(get_json().content)
+    for report in reports:
+        print(report['id'] + '\t' + report['site'])
+
+
+def parse_args(args):
+    if args.list:
+        print_stations_list()
+        sys.exit(EXIT_SUCCESS)
+    stations = [] if args.all else args.station
+    return 'metar', stations
