4a6b79578bafb30c901eb5662b081ea57620071f
Vladimir Vrzić <vvrzic@gmail.com>
2021-03-22 10:47:24 +0100

    Add new condition to hidmet backend
    Суснежица => CodeLightSnow

--- backend/hidmet.py
+++ backend/hidmet.py
@@ -63,6 +63,7 @@ def get_condition_code(language, condition):
             'Умерена киша':         'CodeHeavyRain',
             'Росуља (сипећа киша)': 'CodeLightRain',
             'Сумаглица':            'CodeFog',
+            'Суснежица':            'CodeLightSnow',
             'Слаб снег':            'CodeLightSnow',
             'Умерен снег':          'CodeHeavySnow',
             'Снежна мећава':        'CodeHeavySnow'
